4f771ce1e0646f9144703862d2e9c885982e7307
Prosantaroy1 <royprosanta172@gmail.com>
2023-05-03 16:19:25 +0600

    chef data server api

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+const chef = require('./data/chef.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/chef', (req, res) => {
+  res.send(chef);
 })
 
 app.listen(port, () => {
